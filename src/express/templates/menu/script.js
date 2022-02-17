424e0ba0b83daddee71cd6d9b53a82cf2d8fab72
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-02-17 20:52:10 -0300

    feat: unlock and lock tasks

--- src/express/templates/menu/script.js
+++ src/express/templates/menu/script.js
@@ -1,7 +1,35 @@
-function draggableTasks(isDraggable) {
-   let tasks = document.querySelectorAll('.kanban-drag');
+init();
 
-   tasks.forEach((task) => {
-      task.draggable = isDraggable;
+function init() {
+   buttonsHeaderCard();
+}
+
+function buttonsHeaderCard() {
+   let cards = document.querySelectorAll('.card');
+   cards.forEach((card) => {
+      card.children[0].childNodes.forEach((button) => {
+         // adiciona funções de acordo com os botões
+
+         if (button.src?.includes('locked')) {
+            // bloqueia e desbloqueia cards
+            button.addEventListener('click', () => {
+               // troca icone
+               let unlocked = 'http://localhost:3000/assets/unlocked-black.svg';
+               let locked = 'http://localhost:3000/assets/locked-black.svg';
+               button.src.includes('unlocked') ? (button.src = locked) : (button.src = unlocked);
+
+               // função para deixar cards draggable = true
+               card.children[1].childNodes.forEach((task) => {
+                  task.draggable = !task.draggable;
+               });
+            });
+         }
+
+         if (button.src?.includes('minimize')) {
+            button.addEventListener('click', () => {
+               console.log('minimize');
+            });
+         }
+      });
    });
 }
