fddc513221db57ee11e2eae3e248efa31e7b22fb
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-02-22 20:01:26 -0300

    fix: db url

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,14 +1,11 @@
 import { ServiceAccount } from 'firebase-admin';
 import * as admin from 'firebase-admin';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 export function initFirebase() {
    const adminConfig: ServiceAccount = JSON.parse(process.env.FIREBASE as string);
 
    admin.initializeApp({
       credential: admin.credential.cert(adminConfig),
-      databaseURL: 'https://feliz-2022-default-rtdb.firebaseio.com/',
+      databaseURL: 'https://whats-grafica-default-rtdb.firebaseio.com/',
    });
 }
